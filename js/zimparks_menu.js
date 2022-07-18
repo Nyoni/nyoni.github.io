877957be2702cf2b59a9524cd684f5f086409ffd
Nyoni <tafnyoni123@gmail.com>
2022-07-18 00:25:38 -0700

    UPDATE

---
+++ js/zimparks_menu.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    "use strict";
+    $('.zimparks_menu > ul > li:has( > ul)').addClass('zimparks_menu-dropdown-icon');
+    $('.zimparks_menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+    $(".zimparks_menu > ul").before("<a href=\"#\" class=\"zimparks_menu-mobile\">&nbsp;</a>");
+    $(".zimparks_menu > ul > li").hover(function(e) {
+      if ($(window).width() > 943) {
+        $(this).children("ul").stop(true, false).fadeToggle(150);
+        e.preventDefault();
+      }
+    });
+    $(".zimparks_menu > ul > li").click(function() {
+      if ($(window).width() <= 943) {
+        $(this).children("ul").fadeToggle(150);
+      }
+    });
+    $(".zimparks_menu-mobile").click(function(e) {
+      $(".zimparks_menu > ul").toggleClass('show-on-mobile');
+      e.preventDefault();
+    });
+  });
+  $(window).resize(function() {
+    $(".zimparks_menu > ul > li").children("ul").hide();
+    $(".zimparks_menu > ul").removeClass('show-on-mobile');
+  });
