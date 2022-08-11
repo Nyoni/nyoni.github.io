05fed86643ea67d40bf39ce7768645c19acd1d99
Nyoni <tafnyoni123@gmail.com>
2022-08-11 22:58:09 +0200

    update

--- js/parks_final.js
+++ js/parks_final.js
@@ -1,10 +1,10 @@
 $(function(){
 
     var tabs = function() {
-        $('#parks').css('height', $('.tab-content.active').height() + 600);
+        $('#parks').css('height', $('.tab-content.active').height() + 400);
   
         $(window).resize(function(){
-           $('#parks').css('height', $('.tab-content.active').height() + 600);
+           $('#parks').css('height', $('.tab-content.active').height() + 400);
         });
   
         $('.tabs-nav > a').on('click', function(e){
@@ -18,7 +18,7 @@ $(function(){
            
            setTimeout(function(){
               $('.tab-content[data-tab-content="'+tab+'"]').addClass('active');
-              $('#parks').css('height', $('.tab-content.active').height() + 600);
+              $('#parks').css('height', $('.tab-content.active').height() + 400);
            }, 200);
            setTimeout(function(){
               $('.tab-content[data-tab-content="'+tab+'"]').addClass('show');
